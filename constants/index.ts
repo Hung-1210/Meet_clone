5a02af20ae053dc4407dcbddc0232f330077043d
Hung-1210 <hungnd.22it@vku.udn.vn>
2024-12-08 16:46:05 +0700

    Add new files and folders

---
+++ constants/index.ts
@@ -0,0 +1,36 @@
+export const sidebarLinks = [
+    {
+      imgURL: '/icons/Home.svg',
+      route: '/',
+      label: 'Trang chủ',
+    },
+  
+    {
+      imgURL: '/icons/upcoming.svg',
+      route: '/upcoming',
+      label: 'Cuộc họp sắp tới',
+    },
+    {
+      imgURL: '/icons/previous.svg',
+      route: '/previous',
+      label: 'Cuộc họp đã diễn ra',
+    },
+    {
+      imgURL: '/icons/Video.svg',
+      route: '/recordings',
+      label: 'Bản ghi cuộc họp',
+    },
+    {
+      imgURL: '/icons/add-personal.svg',
+      route: '/personal-room',
+      label: 'Trang cá nhân',
+    },
+  ];
+
+  export const avatarImages = [
+    '/images/avatar.svg',
+    '/images/avatar.svg',
+    '/images/avatar.svg',
+    '/images/avatar.svg',
+    '/images/avatar.svg',
+  ];
